291263957cfca93745a9cef5f32698e90553973b
wangaimin <448497795@qq.com>
2017-02-07 16:52:27 +0800

    添加Js继承

---
+++ Wam.Project/Wam.Project/Controllers/JsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wam.Project.Controllers
+{
+    public class JsController : Controller
+    {
+        // GET: Js
+        /// <summary>
+        /// 构造函数的继承
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 非构造函数的继承
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
