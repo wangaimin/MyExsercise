b9e8acd75395e33da9a638ed08f288866ef009f8
wangaimin <448497795@qq.com>
2017-02-12 21:07:23 +0800

    添加抽象类继承、单元测试

---
+++ Wam.Project/Wam.Utility/child.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wam.Utility
+{
+  public  class child:parent
+    {
+        public child() {
+            age=25;
+        }
+        public int age { get; set; }
+        public string work { get ; set; }
+    }
+}
---
+++ Wam.Project/Wam.Utility/parent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wam.Utility
+{
+    public abstract class parent
+    {
+
+        public parent()
+        {
+            name = "gene";
+
+        }
+
+        public string name;
+
+      
+        
+
+        public string address {
+            get;set;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void setAddress(string address)
+        {
+            this.address = address;
+        }
+    }
+}
